718209660c55d47579cbf1284827fe968fcbf909
Alexandros aristeridis@hotmail.gr <aristeridis@hotmail.gr>
2024-07-30 15:34:46 +0300

    initial commit

---
+++ Technikon/src/main/java/gr/ed/technikon/Technikon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gr.ed.technikon;
+
+/**
+ *
+ * @author alexandrosaristeridis
+ */
+public class Technikon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
