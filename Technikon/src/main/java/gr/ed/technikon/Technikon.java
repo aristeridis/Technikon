8cb59648d5852aad6306b81128b347090ee809e5
Your NameElenaChaidou <helenchaidou1@gmail.com>
2024-07-30 15:43:56 +0300

    initial commit

--- Technikon/src/main/java/gr/ed/technikon/Technikon.java
+++ Technikon/src/main/java/gr/ed/technikon/Technikon.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package gr.ed.technikon;
 
-/**
- *
- * @author alexandrosaristeridis
- */
 public class Technikon {
 
     public static void main(String[] args) {
