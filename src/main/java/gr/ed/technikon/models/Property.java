bdf1da92b817f85abc00dca9799383f11aabea98
Your NameElenaChaidou <helenchaidou1@gmail.com>
2024-07-31 14:28:10 +0300

    add code in Property class

--- src/main/java/gr/ed/technikon/models/Property.java
+++ src/main/java/gr/ed/technikon/models/Property.java
@@ -1,5 +1,71 @@
 package gr.ed.technikon.models;
 
-public class Property {
+import gr.ed.technikon.enums.PropertyType;
+
+public class Property implements PersistentClass {
+
+    private int propertyId;
+    private String address;
+    private int yearOfConstruction;
+    private PropertyType propertyType;
+    private Owner owner;
+
+    public Property(int propertyId, String address, int yearOfConstruction, PropertyType propertyType, Owner owner) {
+        this.propertyId = propertyId;
+        this.address = address;
+        this.yearOfConstruction = yearOfConstruction;
+        this.propertyType = propertyType;
+        this.owner = owner;
+    }
+
+    public int getPropertyId() {
+        return propertyId;
+    }
+
+    public void setPropertyId(long propertyId) {
+        this.propertyId = propertyId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getYearOfConstruction() {
+        return yearOfConstruction;
+    }
+
+    public void setYearOfConstruction(int yearOfConstruction) {
+        this.yearOfConstruction = yearOfConstruction;
+    }
+
+    public PropertyType getPropertyType() {
+        return propertyType;
+    }
+
+    public void setPropertyType(PropertyType propertyType) {
+        this.propertyType = propertyType;
+    }
+
     
+    public long getOwnerVatNumber() {
+        return owner.getVatNumber();
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Property{" + "propertyId=" + propertyId + ", address=" + address + ", yearOfConstruction=" + yearOfConstruction + ", propertyType=" + propertyType + ", owner=" + owner + '}';
+    }
+
 }
