eb374f9d82dc4b61e98189469c98f5d5f0b0c300
Your NameElenaChaidou <helenchaidou1@gmail.com>
2024-08-02 22:12:22 +0300

    add PropertyRepository

--- src/main/java/gr/ed/technikon/Repositories/OwnerRepositoryInterface.java
+++ src/main/java/gr/ed/technikon/Repositories/OwnerRepositoryInterface.java
@@ -3,7 +3,7 @@ package gr.ed.technikon.Repositories;
 import java.util.List;
 import java.util.Optional;
 
-public interface RepositoryInterface<T, K, S> {
+public interface OwnerRepositoryInterface<T, K, S> {
     Optional<T> findByVatNumber(K vt);
     Optional<T>   findByEmail(S s);
     List<T> findAll();
