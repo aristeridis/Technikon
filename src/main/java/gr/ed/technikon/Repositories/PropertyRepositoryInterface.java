37817c26241f0cb0f02004e34ff85d54122e7d54
Your NameElenaChaidou <helenchaidou1@gmail.com>
2024-08-02 22:11:50 +0300

    add PropertyRepository

---
+++ src/main/java/gr/ed/technikon/Repositories/PropertyRepositoryInterface.java
@@ -0,0 +1,20 @@
+package gr.ed.technikon.Repositories;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+public interface PropertyRepositoryInterface<T, K> {
+    
+    Optional<T> findById(K id);
+    
+    List<T> findByDateRange(Date startDate, Date endDate);
+
+    List<T> findByOwnerId(K ownerId);
+
+    List<T> findAll();
+
+    Optional<T> save(T property);
+
+    boolean deleteById(K id);
+}
