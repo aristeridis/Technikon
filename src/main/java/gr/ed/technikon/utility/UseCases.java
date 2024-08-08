6f6cd07f9c313004d6af11ae127c37f27450609f
Alexandros aristeridis@hotmail.gr <aristeridis@hotmail.gr>
2024-08-08 13:15:25 +0300

    acceptance usecase finally working

--- src/main/java/gr/ed/technikon/utility/UseCases.java
+++ src/main/java/gr/ed/technikon/utility/UseCases.java
@@ -31,161 +31,159 @@ import java.util.Scanner;
 
 public class UseCases {
 
-    private static final OwnerRepositoryInterface ownerRepo = new OwnerRepository();
-    private static final PropertyRepositoryInterface propertyRepo = new PropertyRepository();
-    private static final RepairRepositoryInterface repairRepo = new RepairRepository();
+	private static final OwnerRepositoryInterface ownerRepo = new OwnerRepository();
+	private static final PropertyRepositoryInterface propertyRepo = new PropertyRepository();
+	private static final RepairRepositoryInterface repairRepo = new RepairRepository();
 
-    private static final IOServiceInterface ioService = new IOService(ownerRepo, propertyRepo, repairRepo);
-    private static final OwnerServiceInterface ownerService = new OwnerService(propertyRepo);
-    private static final AdminServiceInterface adminService = new AdminService();
+	private static final IOServiceInterface ioService = new IOService(ownerRepo, propertyRepo, repairRepo);
+	private static final OwnerServiceInterface ownerService = new OwnerService(propertyRepo);
+	private static final AdminServiceInterface adminService = new AdminService();
 
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
-    private static Property property;
-    private static Owner owner;
-    private static Repair repair;
-    private static final Scanner scanner = new Scanner(System.in);
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
+	private static Property property;
+	private static Owner owner;
+	private static Repair repair;
+	private static final Scanner scanner = new Scanner(System.in);
 
-    public static void dataPopulation() {
-        System.out.println("|-------------------4.1-------------------|");
-        System.out.println("|-------------------START-------------------|");
+	public static void dataPopulation() {
+		System.out.println("|-------------------4.1-------------------|");
 
-        System.out.println();
-        System.out.println("|-------------------Read Data from csv files-------------------|");
-        ioService.readOwnersCsv("data" + separator + "owner.csv");
-        ioService.readPropertiesCsv("data" + separator + "property.csv");
-        ioService.readRepairsFromCsv("data" + separator + "repair.csv");
+		ioService.readOwnersCsv("data" + separator + "owner.csv");
+		ioService.readPropertiesCsv("data" + separator + "property.csv");
+		ioService.readRepairsFromCsv("data" + separator + "repair.csv");
 
-        System.out.println();
-        System.out.println("|-------------------Reading Completing-------------------|");
-    }
+		System.out.println();
+	}
 
-    public static void ownerWithTwoProperties() {
-        System.out.println("|-------------------4.2-------------------|");
-        owner = new Owner();
-        owner.setVatNumber(123456789L);
-        owner.setName("Nikos");
-        owner.setSurName("Koukos");
-        owner.setAddress("Kleanthous 14");
-        owner.setPhoneNumber("6978756512");
-        owner.setEmail("nikosk@gmail.com");
-        owner.setUsername("NickKouk");
-        owner.setPassword("12345678");
+	public static void ownerWithTwoProperties() {
+		System.out.println("|-------------------4.2-------------------|");
+		owner = new Owner();
+		owner.setVatNumber(123456789L);
+		owner.setName("Nikos");
+		owner.setSurName("Koukos");
+		owner.setAddress("Kleanthous 14");
+		owner.setPhoneNumber("6978756512");
+		owner.setEmail("nikosk@gmail.com");
+		owner.setUsername("NickKouk");
+		owner.setPassword("12345678");
 
-        ownerRepo.save(owner);
+		ownerRepo.save(owner);
 
-        Property property1 = new Property(123456789L, "Kleanthous 4", 1992, PropertyType.MAISONETTE, owner);
-        Property property2 = new Property(321456789L, "Kleanthous 23", 1996, PropertyType.APARTMENT_BUILDING, owner);
+		Property property1 = new Property(123456789L, "Kleanthous 4", 1992, PropertyType.MAISONETTE, owner);
+		Property property2 = new Property(321456789L, "Kleanthous 23", 1996, PropertyType.APARTMENT_BUILDING, owner);
 
-        List<Property> koukosProperties = new ArrayList();
-        koukosProperties.add(property1);
-        koukosProperties.add(property2);
-        owner.setPropertyList(koukosProperties);
+		List<Property> koukosProperties = new ArrayList();
+		koukosProperties.add(property1);
+		koukosProperties.add(property2);
+		owner.setPropertyList(koukosProperties);
 
-        System.out.println(owner.toString());
-        for (Property property : owner.getPropertyList()) {
-            System.out.println("PropertyCode:" + property.getPropertyCode() + " Address: " + property.getAddress());
+		System.out.println(owner.toString());
+		for (Property property : owner.getPropertyList()) {
+			System.out.println("PropertyCode:" + property.getPropertyCode() + " Address: " + property.getAddress());
 
-            propertyRepo.save(property1);
-            propertyRepo.save(property2);
-        }
+			propertyRepo.save(property1);
+			propertyRepo.save(property2);
+		}
 
-    }
+	}
 
-    public static Repair repairsForPropertiesOwner(long propertyId) throws ParseException {
-        System.out.println("|-------------------4.3-------------------|");
-        Optional<Property> propertyOwner = propertyRepo.findById(propertyId);
-        property = propertyOwner.get();
-        repair = new Repair();
-        repair.setDateOfSubmission(DATE_FORMAT.parse("19-09-2024"));
+	public static Repair repairsForPropertiesOwner(long propertyId) throws ParseException {
+		System.out.println("|-------------------4.3-------------------|");
+		Optional<Property> propertyOwner = propertyRepo.findById(propertyId);
+		property = propertyOwner.get();
+		repair = new Repair();
+		repair.setDateOfSubmission(DATE_FORMAT.parse("19-09-2024"));
 //        repair.setDeletedRepair(false);
-        repair.setDescriptionOfWork("Painting of the living room walls in shades of grey.");
-        repair.setShortDescription("Wall Painting");
-        repair.setRepairType(RepairType.PAINTING);
-        repair.setRepairStatus(RepairStatus.PENDING);
-        repair.setProperty(property);
+		repair.setDescriptionOfWork("Painting of the living room walls in shades of grey.");
+		repair.setShortDescription("Wall Painting");
+		repair.setRepairType(RepairType.PAINTING);
+		repair.setRepairStatus(RepairStatus.PENDING);
+		repair.setProperty(property);
 
-        repairRepo.save(repair);
-        return repair;
-    }
+		repairRepo.save(repair);
+		return repair;
+	}
 
-    public static void selectPropertiesFromOwnerById(long ownerId) {
-        Optional<Owner> ownerProperty = ownerRepo.findByOwnerId(ownerId);
+	public static void selectPropertiesFromOwnerById(long ownerId) {
+		Optional<Owner> ownerProperty = ownerRepo.findByOwnerId(ownerId);
 //          owner = ownerProperty.get();
-        OwnerService ownerService = new OwnerService(propertyRepo);
-        for (Property property : ownerService.getPropertiesByOwnerId(ownerId)) {
-            System.out.println("The property ID for this unique ownerID is/are: " + property.getPropertyId());
-        }
+		OwnerService ownerService = new OwnerService(propertyRepo);
+		for (Property property : ownerService.getPropertiesByOwnerId(ownerId)) {
+			System.out.println("The property ID for this unique ownerID is/are: " + property.getPropertyId());
+		}
 
-    }
+	}
 //      Den apothikeuetai sth vash
 
-    public static boolean ownerAcceptanceOfRepairs(Boolean b, long repairId) throws ParseException {
-        System.out.println("|-------------------4.4-------------------|");
-        OwnerService ownerService = new OwnerService(propertyRepo);
-        Optional<Repair> repair = repairRepo.findById(repairId);
-        Repair repairObj = repair.get();
-        Boolean rp = ownerService.acceptance(repairObj);
-        repairRepo.save(repairObj);
-        return rp;
+	public static boolean ownerAcceptanceOfRepairs(long repairId) throws ParseException {
+		System.out.println("|-------------------4.4-------------------|");
+		OwnerService ownerService = new OwnerService(propertyRepo);
+		Optional<Repair> repair = repairRepo.findById(repairId);
+		if (repair.isEmpty()) {
+			return false;
+		}
+		Repair repairObj = repair.get();
 
-    }
+		Boolean rp = ownerService.acceptance(repairObj);
+		repairRepo.save(repairObj);
+		return rp;
 
-    public static void adminGetsPendingRepairs() {
-        AdminService adminService = new AdminService();
-        for (Repair rs : adminService.getPendingRepairs()) {
-            System.out.println("Pending Repairs" + rs.toString());
-        }
-    }
-    
-    public static void adminGetsAllPendingRepairsWithProposedCostAndDates() throws ParseException {
-         System.out.println("|-------------------The Cost, Start and End Dates for Pending Repairs-------------------|");
+	}
 
-        List<Repair> pendingRepairs = adminService.getPendingRepairs();
+	public static void adminGetsPendingRepairs() {
+		AdminService adminService = new AdminService();
+		for (Repair rs : adminService.getPendingRepairs()) {
+			System.out.println("Pending Repairs" + rs.toString());
+		}
+	}
 
-        for (Repair repair : pendingRepairs) {
-            Date proposedStartDate = repair.getProposedDateOfStart();
-            Date proposedEndDate = repair.getProposedDateOfEnd();
-            BigDecimal proposedCost = repair.getProposedCost();
+	public static void adminGetsAllPendingRepairsWithProposedCostAndDates() throws ParseException {
+		List<Repair> pendingRepairs = adminService.getPendingRepairs();
 
-            System.out.println("Repair ID: " + repair.getRepairId());
-            System.out.println("Short Description: " + repair.getShortDescription());
-            System.out.println("Description of Work: " + repair.getDescriptionOfWork());
-            System.out.println("Proposed Cost: " + proposedCost);
+		for (Repair repair : pendingRepairs) {
+			Date proposedStartDate = repair.getProposedDateOfStart();
+			Date proposedEndDate = repair.getProposedDateOfEnd();
+			BigDecimal proposedCost = repair.getProposedCost();
 
-            if (proposedStartDate != null && proposedEndDate != null) {
-                System.out.println("Proposed Start Date: " + DATE_FORMAT.format(proposedStartDate));
-                System.out.println("Proposed End Date: " + DATE_FORMAT.format(proposedEndDate));
-            } else {
-                System.out.println("Proposed Dates: Not set.");
-            }
-            
-        }
-    }
+			System.out.println("Repair ID: " + repair.getRepairId());
+			System.out.println("Short Description: " + repair.getShortDescription());
+			System.out.println("Description of Work: " + repair.getDescriptionOfWork());
+			System.out.println("Proposed Cost: " + proposedCost);
 
-    private Date parseDate(String dateStr) throws ParseException {
-        Date date = DATE_FORMAT.parse(dateStr);
-        return date;
-    }
+			if (proposedStartDate != null && proposedEndDate != null) {
+				System.out.println("Proposed Start Date: " + DATE_FORMAT.format(proposedStartDate));
+				System.out.println("Proposed End Date: " + DATE_FORMAT.format(proposedEndDate));
+			} else {
+				System.out.println("Proposed Dates: Not set.");
+			}
 
-    private long parseLong(String value) {
-        try {
-            return Long.parseLong(value.trim());
-        } catch (NumberFormatException e) {
-            return -1; 
-        }
-    }
+		}
+	}
 
-    private int parseInt(String value) {
-        try {
-            return Integer.parseInt(value.trim());
-        } catch (NumberFormatException e) {
+	private Date parseDate(String dateStr) throws ParseException {
+		Date date = DATE_FORMAT.parse(dateStr);
+		return date;
+	}
 
-            return -1;
-        }
-    }
+	private long parseLong(String value) {
+		try {
+			return Long.parseLong(value.trim());
+		} catch (NumberFormatException e) {
+			return -1;
+		}
+	}
 
-    private String formatDate(Date date) {
-        return date == null ? "" : DATE_FORMAT.format(date);
-    }
+	private int parseInt(String value) {
+		try {
+			return Integer.parseInt(value.trim());
+		} catch (NumberFormatException e) {
+
+			return -1;
+		}
+	}
+
+	private String formatDate(Date date) {
+		return date == null ? "" : DATE_FORMAT.format(date);
+	}
 
 }
